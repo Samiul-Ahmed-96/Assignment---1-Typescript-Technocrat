039d5cc61ed90919a478231f443e940d3c66d8ad
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2024-09-29 22:31:15 +0600

    Problem 7

--- index.ts
+++ index.ts
@@ -147,3 +147,42 @@ function sumArrayIfNumbers(data: unknown): void {
 sumArrayIfNumbers([1, 2, 3, 4]); // Output: Sum of numbers: 10
 sumArrayIfNumbers(["a", 2, 3]); // Output: Error: Input is not an array of numbers.
 sumArrayIfNumbers(42); // Output: Error: Input is not an array of numbers.
+
+// Problem 7
+
+// Create a TypeScript function called findFirstOccurrence that accepts an array and a value to search for. Use generics to make the function work with arrays of any data type. Inside the function, find and return the index of the first occurrence of the value in the array. If the value is not found in the array, return -1 to indicate that. Create sample arrays of different data types (e.g., numbers, strings) and call the findFirstOccurrence function for each of them to display the results.
+
+// // Example usage:
+
+// const numbers: number[] = [1, 2, 3, 4, 5, 2];
+
+// const strings: string[] = ["apple", "banana", "cherry", "date", "apple"];
+
+// const targetNumber = 2;
+
+// const targetString = "cherry";
+
+// const indexInNumbers = findFirstOccurrence(numbers, targetNumber);
+
+// const indexInStrings = findFirstOccurrence(strings, targetString);
+
+// console.log(indexInNumbers); //output:  1
+
+// console.log(indexInStrings); //output: 2
+
+function findFirstOccurrence<T>(arr: T[], value: T): number {
+  return arr.indexOf(value);
+}
+
+// Example usage:
+const numbers: number[] = [1, 2, 3, 4, 5, 2];
+const strings: string[] = ["apple", "banana", "cherry", "date", "apple"];
+
+const targetNumber = 2;
+const targetString = "cherry";
+
+const indexInNumbers = findFirstOccurrence(numbers, targetNumber);
+const indexInStrings = findFirstOccurrence(strings, targetString);
+
+console.log(indexInNumbers); // Output: 1
+console.log(indexInStrings); // Output: 2
